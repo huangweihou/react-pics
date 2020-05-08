b78cb24308e2ea4ffd4a99e3dac4d37e61077028
peter <peter@peterde-MacBook-Pro.local>
2020-05-08 22:40:55 +0800

    Updates

---
+++ precache-manifest.eb988429712eebfc1bbae8f6fc1e241c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb6aea08814a1d9d72fbf9e83f3dc26",
+    "url": "/react-pics/index.html"
+  },
+  {
+    "revision": "801a24f33309409d3fef",
+    "url": "/react-pics/static/css/main.4fa831c1.chunk.css"
+  },
+  {
+    "revision": "9649f367853cdd0dfc2d",
+    "url": "/react-pics/static/js/2.2f9b5b2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pics/static/js/2.2f9b5b2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "801a24f33309409d3fef",
+    "url": "/react-pics/static/js/main.c0ae1f09.chunk.js"
+  },
+  {
+    "revision": "02db5993deb0ce8b5928",
+    "url": "/react-pics/static/js/runtime-main.db167f6e.js"
+  }
+]);
